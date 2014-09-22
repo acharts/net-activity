82e282b33a4c07272e423876f13d099bbea767e6
好修 <hankai.hk@alibaba-inc.com>
2014-09-22 19:19:45 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+var achartNetActivity;
+
+module.exports = achartNetActivity;
---
+++ tests/achart-net-activity-spec.js
@@ -0,0 +1,10 @@
+var expect = require('expect.js');
+var achartNetActivity = require('../index');
+
+describe('achart-net-activity', function() {
+
+  it('normal usage', function() {
+
+  });
+
+});
